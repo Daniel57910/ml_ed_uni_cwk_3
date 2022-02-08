9c78e0e3f374616da8bd5601bce83c4c5beedfb1
DaMiller1 <danielmiller5791@gmail.com>
2022-02-08 14:31:30 +0000

    adding code

--- code/run_slurm.sh
+++ code/run_slurm.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Teach-Standard
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
 export CUDNN_HOME=/opt/cuDNN-7.0/
