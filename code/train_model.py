d787a49e9d51e62ebd1491ca7915f2a3ab8db189
DaMiller1 <danielmiller5791@gmail.com>
2022-02-16 14:06:38 +0000

    sending results to gpu

--- code/train_model.py
+++ code/train_model.py
@@ -58,7 +58,6 @@ model.to(device)
 
 criterion = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-import pdb
 
 epoch = 0
 iteration = 0
@@ -92,29 +91,26 @@ for i in range(0, max_epoch_number):
 
         model.eval()
 
-        """
-        Run against test data
-        """
-        with torch.no_grad():
-            for index_val, (val_imgs, val_targets) in enumerate(test_dataloader):
-                val_imgs, val_targets = val_imgs.to(device), val_targets.to(device)
-                val_result = model(val_imgs)
-                val_losses = criterion(val_result, val_targets)
-                val_metrics = calculate_metrics(
-                    val_result.cpu().numpy(),
-                    val_targets.cpu().numpy()
-                )
+    """
+    Run against test data
+    """
+    with torch.no_grad():
+        for index_val, (val_imgs, val_targets) in enumerate(test_dataloader):
+            val_result = model(val_imgs)
+            val_losses = criterion(val_result, val_targets)
+            val_metrics = calculate_metrics(
+                val_result.cpu().numpy(),
+                val_targets.cpu().numpy()
+            )
 
-                val_metrics['epoch'] = i
-                val_metrics['losses'] = val_losses.item()
-                batch_losses_test.append(val_metrics)
-
+            val_metrics['epoch'] = i
+            val_metrics['losses'] = val_losses.item()
+            batch_losses_test.append(val_metrics)
 
     print(f"Batch training losses at {i} {batch_losses[-1]}")
     print(f"Batch validation losses at {i} {batch_losses_test[-1]}")
 
 time = datetime.now().strftime("%Y_%m_%d-%H:%M")
-
 df = pd.DataFrame(batch_losses)
 df_val = pd.DataFrame(batch_losses_test)
 
