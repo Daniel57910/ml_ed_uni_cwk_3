64cabe8cebcd5ed6b2e2dbe4cb9ca0b2aec268f0
DaMiller1 <danielmiller5791@gmail.com>
2022-02-17 11:51:56 +0000

    adding code

--- code/evaluate_results.py
+++ code/evaluate_results.py
@@ -1,11 +1,60 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+RESULTS_FILES_DIR = 'results_files'
 
-df = pd.read_csv("training_results_2022_02_15-19:34.csv")
-df = df.groupby('epoch').mean()
-print(df.columns)
-df['micro/recall'].plot(
-    kind='line',
-    title='Recall Per Epoch On Training Data',
-    y='Binary Cross Entropy')
+
+df_train, df_val = (
+    pd.read_csv(RESULTS_FILES_DIR + "/training_results_2022_02_16-15:03.csv"),
+    pd.read_csv(RESULTS_FILES_DIR + "/validation_results_2022_02_16-15:03.csv")
+)
+
+df_train, df_val = (
+    df_train.groupby('epoch').mean().reset_index(),
+    df_val.groupby('epoch').mean().reset_index()
+)
+
+df_train.columns = [col + "_train"  if col != 'epoch' else col for col in df_train.columns]
+df_val.columns = [col + "_val" if col != 'epoch' else col for col in df_val.columns]
+print(df_train.columns)
+
+# print(df_train.count())
+# print(df_val.count())
+
+df_combined = pd.merge(
+    df_train,
+    df_val,
+    on='epoch'
+)
+
+print(df_combined.columns)
+# losses_subset = df_combined[['losses_train', 'losses_val']]
+# print(losses_subset)
+fig, axs = plt.subplots(2, 2)
+plotting_meta = [
+    {
+        "cols": ['losses_train', 'losses_val'],
+        "title": "BCE on training and validation dataset"
+    },
+    {
+        "cols": ["micro/precision_train", "micro/precision_val"],
+        "title": "Precision score on training and validation dataset"
+    },
+    {
+        "cols": ["micro/recall_train", "micro/recall_val"],
+        "title": "Recall score on training and validation dataset"
+    },
+    {
+        "cols": ["micro/f1_train", "micro/f1_val"],
+        "title": "F1 score on training and validation dataset"
+    }
+]
+for ax, meta in zip(axs.flatten(), plotting_meta):
+    subset = df_combined[meta['cols']]
+    subset.plot(
+        ax=ax,
+        title=meta['title']
+    )
+
+    ax.set_xlabel("Epoch")
+
 plt.show()
