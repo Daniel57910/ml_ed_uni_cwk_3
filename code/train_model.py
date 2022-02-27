b1041827bad1ab39acad1a41c729376576c29624
DaMiller1 <danielmiller5791@gmail.com>
2022-02-27 12:04:54 +0000

    adding updated labels

--- code/train_model.py
+++ code/train_model.py
@@ -48,7 +48,7 @@ learning_rate = weight_decay = 1e-4 # Learning rate and weight decay
 max_epoch_number = 35 # Number of epochs for training
 dist.init_process_group(backend='nccl')
 NUM_CLASSES = 81
-BATCH_SIZE=200
+BATCH_SIZE=60
 save_path = 'chekpoints/'
 
 dataset_val = NusDataset(
