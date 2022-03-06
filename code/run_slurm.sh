1fa65464707b467ca409f85085ea8db124d7e141
DaMiller1 <danielmiller5791@gmail.com>
2022-03-06 11:35:06 +0000

    resnet 18 branch setup

--- code/run_slurm.sh
+++ code/run_slurm.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #SBATCH -N 4	  # nodes requested
 #SBATCH -n 4	  # tasks requested
-#SBATCH --gres=gpu:1
 #SBATCH --mem=10000  # memory in Mb
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=Teach-Standard
+#SBATCH --partition=Teach-LongJobs
 #SBATCH --mail-user=s0905577
 #SBATCH --mail-type=ALL
 
@@ -34,4 +33,4 @@ mkdir -p ${TMP}/datasets/
 export DATASET_DIR=${TMP}/datasets/
 # Activate the relevant virtual environment:
 
-source ~/.bashrc && conda activate mlp3 && torchrun cswk_3/code/train_model.py -m resnet_18 -f resnet_18
+source ~/.bashrc && conda activate mlp3 && torchrun ml_ed_uni_cwk_3/code/train_model.py -m resnet_18 -f resnet_18
--- code/train_model.py
+++ code/train_model.py
@@ -74,7 +74,7 @@ def load_data(train_path, test_path):
     return train_dataloader, test_dataloader
 
 LEARNING_RATE = WEIGHT_DECAY = 1e-4
-MAX_EPOCH_NUMBER = 2
+MAX_EPOCH_NUMBER = 70
 NUM_CLASSES = 81
 # BATCH_SIZE=110
 BATCH_SIZE=30
