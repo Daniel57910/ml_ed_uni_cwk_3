7b0a91b725aabd04c70b3aadfa9869be145d9191
DaMiller1 <danielmiller5791@gmail.com>
2022-03-06 11:14:52 +0000

    adding resnet

--- code/resnet_models.py
+++ code/resnet_models.py
@@ -1,9 +1,16 @@
 from webbrowser import get
 import torch.hub as hub
 import torch.nn as nn
-
+from torch import save, load
+import os
 def get_resnet_18(num_classes):
-    model = hub.load('pytorch/vision:v0.10.0', 'resnet18')
+    if not os.path.exists("code/models/resnet_18"):
+        model = hub.load('pytorch/vision:v0.10.0', 'resnet18')
+    else:
+        model = load("code/models/resnet_18")
+        print("Loading untrained model from disk")
+
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, num_classes)
     return model
+
