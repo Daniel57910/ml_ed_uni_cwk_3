10327e3e6762c70ddc56353fbcaf8b2c5ca16513
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 22:39:56 +0000

    Adding tarfile member sanitization to extractall()

--- code/obtain_data.py
+++ code/obtain_data.py
@@ -65,5 +65,24 @@ if not os.path.exists(img_folder):
     download_file_from_google_drive(file_id, path_to_tar_file)
     print('Extraction')
     with tarfile.open(path_to_tar_file) as tar_ref:
-        tar_ref.extractall(os.path.dirname(img_folder))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar_ref, os.path.dirname(img_folder))
     os.remove(path_to_tar_file)
